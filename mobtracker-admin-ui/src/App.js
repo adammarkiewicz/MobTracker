2d0943fd9a2d8e8641130fe2297437dec324f12c
Adam Markiewicz <adam.markiewicz@wp.pl>
2020-03-09 20:25:28 +0100

    Connecting with Auth0.

--- mobtracker-admin-ui/src/App.js
+++ mobtracker-admin-ui/src/App.js
@@ -1,9 +1,53 @@
 import React from "react";
-import "./App.css";
-import * as signalR from "@aspnet/signalr";
+import "bulma/css/bulma.css";
+import { useAuth0 } from "./contexts/auth0-context";
+import Header from "./components/Header";
+// import * as signalR from "@aspnet/signalr";
 
 function App() {
-  const but = document.createElement("button");
+  const { isLoading, user, loginWithRedirect, logout } = useAuth0();
+
+  return (
+    <>
+      <Header />
+
+      <div className="hero is-info is-fullheight">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            {!isLoading && !user && (
+              <>
+                <h1>Click Below!</h1>
+                <button
+                  onClick={loginWithRedirect}
+                  className="button is-danger"
+                >
+                  Login
+                </button>
+              </>
+            )}
+            {!isLoading && user && (
+              <>
+                <h1>You are logged in!</h1>
+                <p>Hello {user.name}</p>
+
+                {user.picture && <img src={user.picture} alt="My Avatar" />}
+                <hr />
+
+                <button
+                  onClick={() => logout({ returnTo: window.location.origin })}
+                  className="button is-small is-dark"
+                >
+                  Logout
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+
+  /* const but = document.createElement("button");
   but.innerText = "Click";
   but.onclick = function changeContent() {
     fetch("/test")
@@ -38,7 +82,7 @@ function App() {
     console.log("Sending location request!");
     connection.invoke("GetLocation");
   });
-  return <div id="app" className="App"></div>;
+  return <div id="app" className="App"></div>; */
 }
 
 export default App;
